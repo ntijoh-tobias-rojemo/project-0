363462d1c63605719470560a2e991419ec0e751e
Tobias <tobias.rojemo@elev.ga.ntig.se>
2023-08-17 14:18:09 +0200

    added routes for content pages

--- app.rb
+++ app.rb
@@ -3,5 +3,13 @@ class App < Sinatra::Base
     get '/' do
         "Hello, world!"
     end
+
+    get '/images' do
+        erb("images")
+    end
+
+    get '/people' do
+        erb("people")
+    end
     
 end
