671ef8e43a86ae60ee2e18aed75002f63ee9d972
Tobias <tobias.rojemo@elev.ga.ntig.se>
2023-08-18 12:41:48 +0200

    cleaned up client side code

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
+// Unpack datastring
 const data = document
   .getElementById("data")
   .innerHTML.split("§§")
@@ -5,21 +6,40 @@ const data = document
     return { image: a.split("§")[0], name: a.split("§")[1] };
   });
 
+// get elements
 const display = document.getElementById("image-display");
 const input = document.getElementById("input-field");
 const button = document.getElementById("submit-button");
 const counter = document.getElementById("counter");
 
+// returns a random element from the people left and removes it from the array
+const getNext = () => left.splice((Math.random() * left.length) | 0, 1)[0];
+
+// updates elements on the page
+function updatePage() {
+  display.src = current.image;
+  counter.innerHTML = correct;
+  input.value = "";
+}
+
+// setup
 var correct = 0;
 const total = data.length;
+document.getElementById("total").innerHTML = total;
 
 var left = [...data];
 var current = getNext();
 updatePage();
 
-document.getElementById("total").innerHTML = total;
+// each time the button is clicked or enter is pressed in the textbox, run the page logic
+button.addEventListener("click", tick);
+input.addEventListener("keydown", (event) => {
+  if (event.keyCode == 13) {
+    tick();
+  }
+});
 
-button.addEventListener("click", () => {
+function tick() {
   const guess = input.value;
   if (current.name == guess) correct++;
   if (left.length == 0) {
@@ -29,14 +49,4 @@ button.addEventListener("click", () => {
   }
   current = getNext();
   updatePage();
-});
-
-function getNext() {
-  return left.splice((Math.random() * left.length) | 0, 1)[0];
-}
-
-function updatePage() {
-  display.src = current.image;
-  counter.innerHTML = correct;
-  input.value = "";
 }
