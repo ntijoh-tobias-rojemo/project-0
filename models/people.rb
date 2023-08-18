5b9a6238ed70b42031c3be7c9202fe5e0c7804fb
linus-bostrom <linus.bostrom@elev.ga.ntig.se>
2023-08-18 13:08:12 +0200

    Lagt till headers

--- models/people.rb
+++ models/people.rb
@@ -1,15 +1,18 @@
 class People
 
+    #Returnerar samtligt inehåll i databasen.
     def self.all
         db.execute('SELECT * FROM people')
     end
 
+    #Returnerar samtligt inehåll i databsen som en sträng.
     def self.all_as_string
         data = db.execute('SELECT * FROM people')
         return data.map { |person| "#{person["image"]}§#{person["name"]}" }.join("§§")
     end
 
     private
+    #Returnerar en databas om den finns, annars skapas en.
     def self.db
         return @db if @db
         @db = SQLite3::Database.new('db/db.sqlite')
