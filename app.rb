858b79bfc33dfb35a241bbd3440b0f064813240d
Tobias <tobias.rojemo@elev.ga.ntig.se>
2023-08-21 14:07:38 +0200

    routes now query by class

--- app.rb
+++ app.rb
@@ -2,23 +2,23 @@ require_relative 'models/people.rb'
 
 class App < Sinatra::Base
 
-    get '/' do
-        @datastring = People.all_as_string
+    get '/:classid' do |classid|
+        @datastring = People.class_as_string(classid)
         erb :main
     end
 
-    get '/images' do
-        @people = People.all
+    get '/:classid/images' do |classid|
+        @people = People.class(classid)
         erb :images
     end
 
-    get '/people' do
-        @people = People.all
+    get '/:classid/people' do |classid|
+        @people = People.class(classid)
         erb :people
     end
 
-    get '/results/:answers' do |answers|
-        @people = People.all
+    get '/:classid/results/:answers' do |classid, answers|
+        @people = People.class(classid)
         @answers = answers.to_i
         erb :results
     end  
--- models/people.rb
+++ models/people.rb
@@ -2,12 +2,21 @@ class People
 
     #Returnerar samtligt inehåll i databasen.
     def self.all
-        db.execute('SELECT * FROM people')
+        db.execute('SELECT * FROM people ORDER BY id')
     end
 
     #Returnerar samtligt inehåll i databsen som en sträng.
     def self.all_as_string
-        data = db.execute('SELECT * FROM people')
+        data = db.execute('SELECT * FROM people ORDER BY id')
+        return data.map { |person| "#{person["id"]}§#{person["image"]}§#{person["name"]}§#{person["nickname"] || "NO_NICK"}" }.join("§§")
+    end
+
+    def self.class(class_id)
+        db.execute('SELECT * FROM people WHERE classid = ? ORDER BY id', class_id)
+    end
+
+    def self.class_as_string(class_id)
+        data = db.execute('SELECT * FROM people WHERE classid = ? ORDER BY id', class_id)
         return data.map { |person| "#{person["id"]}§#{person["image"]}§#{person["name"]}§#{person["nickname"] || "NO_NICK"}" }.join("§§")
     end
 
